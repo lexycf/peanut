8174c1570b4919179f9a105fb085b8af2a6cef6a
lexycf <43943650+lexycf@users.noreply.github.com>
2019-02-18 18:48:15 +0800

    update

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -4,7 +4,7 @@
     <router-view/>
   </div>
 </template>
-<script src='http://res.wx.qq.com/open/js/jweixin-1.4.0.js'></script>
+
 <script> 
 require('../static/commonfile/css/lib.less');
 require('./utils/weixinport.js');
