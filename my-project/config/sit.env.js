2ca1f698f399d7b1911f95c9702816e7bc653203
lexycf <43943650+lexycf@users.noreply.github.com>
2019-02-22 18:27:41 +0800

    update

--- my-project/config/sit.env.js
+++ my-project/config/sit.env.js
@@ -1,5 +1,5 @@
 module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"sit"',
-  BASE_API: '"http://www.huashenghuzhu.com:8085/sphz/"'
+  BASE_API: '"http://www.huashenghuzhu.com:8085/sphz"'
 }
